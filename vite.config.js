04c66d1ffa9928b11dd601913f777dc974ebc46d
Rohaann <Rohand765@gmail.com>
2026-03-29 14:50:59 +0530

    Restored contact entry point - corrected misidentified file status

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
+                contact: resolve(__dirname, 'contact.html'),
                 work: resolve(__dirname, 'work.html'),
                 brand: resolve(__dirname, 'brand.html'),
             },
